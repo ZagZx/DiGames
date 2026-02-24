728210ed68cd345ae8e745d8a8e40f6cf98e20d3
ZagZx <azevedodvictor@gmail.com>
2026-02-23 21:01:03 -0300

    feat: validação para o minimo de caracteres do nome do jogo

--- backend/src/main/java/com/divateam/digames/dto/jogo/JogoRequest.java
+++ backend/src/main/java/com/divateam/digames/dto/jogo/JogoRequest.java
@@ -1,6 +1,7 @@
 package com.divateam.digames.dto.jogo;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,13 +10,14 @@ import java.util.List;
 
 public record JogoRequest(
         @NotEmpty(message = "Nome não pode ser vazio ou nulo")
+        @Min(value = 3, message = "Nome deve ter pelo menos 3 caracteres")
         String nome,
 
         @NotEmpty(message = "Status não pode ser vazio ou nulo")
         String status,
 
         @NotNull(message = "Preço não pode ser nulo")
-        @DecimalMin(message = "Preço não pode ser negativo", value = "0.0")
+        @DecimalMin(value = "0.0", message = "Preço não pode ser negativo")
         BigDecimal preco,
 
         String descricao,
