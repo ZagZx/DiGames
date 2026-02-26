f024c1eb4664fd83069114bf4cc39b78cd5b95e7
ZagZx <azevedodvictor@gmail.com>
2026-02-25 21:04:03 -0300

    feat: endpoints para deletar e buscar jogo

--- backend/src/main/java/com/divateam/digames/controller/JogoController.java
+++ backend/src/main/java/com/divateam/digames/controller/JogoController.java
@@ -3,9 +3,9 @@ package com.divateam.digames.controller;
 import com.divateam.digames.dto.jogo.JogoRequest;
 import com.divateam.digames.dto.jogo.JogoResponse;
 import com.divateam.digames.service.JogoService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,10 +35,22 @@ public class JogoController {
         return ResponseEntity.ok(jogos);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<JogoResponse> buscarJogo(@PathVariable Long id) {
+        JogoResponse jogoResponse = new JogoResponse(jogoService.buscar(id));
+        return ResponseEntity.ok(jogoResponse);
+    }
+
     @PostMapping
-    public ResponseEntity<JogoResponse> criarJogo(@RequestBody @Valid JogoRequest jogoRequest) {
+    public ResponseEntity<JogoResponse> criarJogo(@RequestBody @Validated JogoRequest jogoRequest) {
         JogoResponse jogoResponse = new JogoResponse(jogoService.criar(jogoRequest));
 
         return new ResponseEntity<>(jogoResponse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarJogo(@PathVariable Long id) {
+        jogoService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/com/divateam/digames/service/JogoService.java
+++ backend/src/main/java/com/divateam/digames/service/JogoService.java
@@ -5,12 +5,17 @@ import com.divateam.digames.entity.Genero;
 import com.divateam.digames.entity.Jogo;
 import com.divateam.digames.repository.GeneroRepository;
 import com.divateam.digames.repository.JogoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class JogoService {
+    private static final Logger log = LoggerFactory.getLogger(JogoService.class);
     private final JogoRepository jogoRepository;
     private final GeneroRepository generoRepository;
 
@@ -21,6 +26,12 @@ public class JogoService {
 
     public List<Jogo> listar() {
         return jogoRepository.findAll();
+    }
+
+    public Jogo buscar(Long id) {
+        return jogoRepository.findById(id).orElseThrow(
+               () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Jogo não encontrado")
+        );
     }
 
     public Jogo criar(JogoRequest jogoRequest) {
@@ -35,5 +46,12 @@ public class JogoService {
         );
 
         return jogoRepository.save(jogo);
+    }
+
+    public void deletar(Long id) {
+        if (!jogoRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Jogo não encontrado");
+        }
+        jogoRepository.deleteById(id);
     }
 }
