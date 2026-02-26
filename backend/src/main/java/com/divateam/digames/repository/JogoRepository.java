2777df780259ee6b372b0088e6c5e829b32ab7aa
ZagZx <azevedodvictor@gmail.com>
2026-02-25 21:48:15 -0300

    feat: método para checar se o jogo existe pelo nome e correção do método de pesquisa por nome

--- backend/src/main/java/com/divateam/digames/repository/JogoRepository.java
+++ backend/src/main/java/com/divateam/digames/repository/JogoRepository.java
@@ -4,7 +4,10 @@ import com.divateam.digames.entity.Jogo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long> {
-    Jogo findByNomeLike(String nome);
+    boolean existsJogoByNome(String nome);
+    List<Jogo> findAllByNomeLike(String nome);
 }
